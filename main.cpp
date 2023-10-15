a9b54a23c1fbe22ebdfeb4744c5c0264662d9e00
RokasMai <146191734+RokasMai@users.noreply.github.com>
2023-10-15 23:29:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
-#include <limits> // Pridėtas includes
+#include <chrono>
 
 int main() {
     char pasirinkimas;
@@ -13,9 +13,16 @@ int main() {
 
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Išvalo buferį
 
+    auto startViso = std::chrono::high_resolution_clock::now(); // Pradedame matuoti laiką
+
     if (pasirinkimas == 'I') {
+        auto startNuskaitymas = std::chrono::high_resolution_clock::now();
         ivestiDuomenisRanka(studentai);
+        auto endNuskaitymas = std::chrono::high_resolution_clock::now();
+        auto durationNuskaitymas = std::chrono::duration_cast<std::chrono::milliseconds>(endNuskaitymas - startNuskaitymas);
+        std::cout << "Duomenu nuskaitymas uztruko " << durationNuskaitymas.count() << "ms" << std::endl;
     } else if (pasirinkimas == 'S') {
+        auto startNuskaitymas = std::chrono::high_resolution_clock::now();
         std::string failoPavadinimas;
         std::cout << "Iveskite failo pavadinima (su pilnu keliasu, jei failas yra kitoje vietoje): ";
         std::cin >> failoPavadinimas;
@@ -23,13 +30,22 @@ int main() {
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Išvalo buferį
 
         skaitytiDuomenisIsFailo(studentai, failoPavadinimas);
-
-        padalintiStudentusIFailus(studentai, "failas_Vargsiukai.txt", "failas_Lyderiai.txt");
+        auto endNuskaitymas = std::chrono::high_resolution_clock::now();
+        auto durationNuskaitymas = std::chrono::duration_cast<std::chrono::milliseconds>(endNuskaitymas - startNuskaitymas);
+        std::cout << "Duomenu nuskaitymas uztruko " << durationNuskaitymas.count() << "ms" << std::endl;
     } else if (pasirinkimas == 'G') {
+        auto startNuskaitymas = std::chrono::high_resolution_clock::now();
         generuotiDuomenis(studentai);
+        auto endNuskaitymas = std::chrono::high_resolution_clock::now();
+        auto durationNuskaitymas = std::chrono::duration_cast<std::chrono::milliseconds>(endNuskaitymas - startNuskaitymas);
+        std::cout << "Duomenu nuskaitymas uztruko " << durationNuskaitymas.count() << "ms" << std::endl;
     } else if (pasirinkimas == 'F') {
+        auto startNuskaitymas = std::chrono::high_resolution_clock::now();
         generuotiFailus();
-        return 0;
+        auto endNuskaitymas = std::chrono::high_resolution_clock::now();
+        auto durationNuskaitymas = std::chrono::duration_cast<std::chrono::milliseconds>(endNuskaitymas - startNuskaitymas);
+        std::cout << "Duomenu nuskaitymas uztruko " << durationNuskaitymas.count() << "ms" << std::endl;
+        return 0;  // Programa baigia darbą po failų generavimo.
     } else {
         std::cout << "Neteisingas pasirinkimas. Programa baigia darbą." << std::endl;
         return 1;
@@ -39,17 +55,29 @@ int main() {
     std::cout << "Pasirinkite skaiciavimo buda (Vid arba Med): ";
     std::cin >> skaiciavimoBudas;
 
+    auto startRusiavimas = std::chrono::high_resolution_clock::now();
     std::sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
         return a.vardas < b.vardas;
     });
+    auto endRusiavimas = std::chrono::high_resolution_clock::now();
+    auto durationRusiavimas = std::chrono::duration_cast<std::chrono::milliseconds>(endRusiavimas - startRusiavimas);
+    std::cout << "Rusiavimas uztruko " << durationRusiavimas.count() << "ms" << std::endl;
 
     std::cout << std::left << std::setw(20) << "Vardas" << std::setw(20) << "Pavarde" << std::setw(20) << skaiciavimoBudas << std::endl;
     std::cout << std::setfill('-') << std::setw(60) << "" << std::setfill(' ') << std::endl;
 
+    auto startIsvedimas = std::chrono::high_resolution_clock::now();
     for (const auto& studentas : studentai) {
         std::cout << std::left << std::setw(20) << studentas.vardas << std::setw(20) << studentas.pavarde
                   << std::fixed << std::setprecision(2) << skaiciuotiGalutiniBala(studentas, skaiciavimoBudas) << std::endl;
     }
+    auto endIsvedimas = std::chrono::high_resolution_clock::now();
+    auto durationIsvedimas = std::chrono::duration_cast<std::chrono::milliseconds>(endIsvedimas - startIsvedimas);
+    std::cout << "Isvedimas uztruko " << durationIsvedimas.count() << "ms" << std::endl;
+
+    auto endViso = std::chrono::high_resolution_clock::now(); // Baigti matuoti laiką
+    auto durationViso = std::chrono::duration_cast<std::chrono::milliseconds>(endViso - startViso);
+    std::cout << "Visas vykdymas uztruko " << durationViso.count() << "ms" << std::endl;
 
     return 0;
 }
