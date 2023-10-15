26ca3281b0da0adcbe1cdb1ac3ee5417889b2110
RokasMai <146191734+RokasMai@users.noreply.github.com>
2023-10-15 19:55:42 +0300

    Add files via upload

--- studentas.h
+++ studentas.h
@@ -1,8 +1,6 @@
 #ifndef STUDENTAS_H_INCLUDED
 #define STUDENTAS_H_INCLUDED
 
-#pragma once
-
 #include <string>
 #include <vector>
 
@@ -17,6 +15,7 @@ double skaiciuotiGalutiniBala(const Studentas& studentas, const std::string& tip
 void ivestiDuomenisRanka(std::vector<Studentas>& studentai);
 void skaitytiDuomenisIsFailo(std::vector<Studentas>& studentai, const std::string& failoPavadinimas);
 void generuotiDuomenis(std::vector<Studentas>& studentai);
-
+void kategorizuotiStudentus(const std::vector<Studentas>& studentai, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietiakiai);
+void isvestiStudentusIFailus(const std::vector<Studentas>& studentai, const std::string& failoPavadinimas);
 
 #endif
